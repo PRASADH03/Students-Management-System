2a11474b1651e4007901308eaa4df162dae196f3
PRASADH03 <kjprasadh03@gmail.com>
2025-11-11 22:51:28 +0530

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Students VALUES (1, 'Keerthana', 'Gunasekaran', 'keerthana@email.com', '2004-05-10');
+INSERT INTO Courses VALUES (101, 'Database Management', 12);
+INSERT INTO Enrollments VALUES (1, 1, 101, 'A');
